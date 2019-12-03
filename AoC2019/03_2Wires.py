7f4b2d5bac7ac6d8a7c02e69983da340630a706b
Ethan Kessel <the.rednecksidekick@gmail.com>
2019-12-03 00:39:48 -0700

    Advent of Code - Day 3.1
    Updated solution to include tqdm progress bar, not that it does much. This is surprisingly fast.

--- AoC2019/03_2Wires.py
+++ AoC2019/03_2Wires.py
@@ -5,6 +5,8 @@ Created on Mon Dec  2 21:54:56 2019
 @author: redne
 """
 
+from tqdm import tqdm # progress bar
+
 indata = input("Enter Input:\n")
 
 wires = indata.split("\n")
@@ -26,7 +28,7 @@ wire_lengths = [0,0]
 
 start_1 = [0,0]
 
-for seg1 in segments[0]:
+for seg1 in tqdm(segments[0],desc="Segment 1 Progress"):
     dir_1 = directions[seg1[0]]
     dst_1 = int(seg1[1:])
     end_1 = [start_1[0] + (dst_1 * dir_1[0]), start_1[1] + (dst_1 * dir_1[1])]
@@ -52,14 +54,14 @@ for seg1 in segments[0]:
             yMin = min(start_2[1], end_2[1])
             # Check if the segments intersect
             if ((xMin < x_2 and x_2 < xMax) and (yMin < y_1 and y_1 < yMax)):
-                print("Horizontal Intersection:", start_1, "->", end_1, "with", start_2, "->", end_2)
+                #print("Horizontal Intersection:", start_1, "->", end_1, "with", start_2, "->", end_2)
                 intersections.append((x_2,y_1))
                 num_intersections += 1
                 extraDst_1 = abs(start_1[0] - x_2)
                 extraDst_2 = abs(start_2[1] - y_1)
-                print("Wire 1 length to bend", wire_lengths[0],"+",extraDst_1)
-                print("Wire 2 length to bend", wire_lengths[1],"+",extraDst_2)
-                print("sum = ", wire_lengths[0] + extraDst_1 + wire_lengths[1] + extraDst_2)
+                #print("Wire 1 length to bend", wire_lengths[0],"+",extraDst_1)
+                #print("Wire 2 length to bend", wire_lengths[1],"+",extraDst_2)
+                #print("sum = ", wire_lengths[0] + extraDst_1 + wire_lengths[1] + extraDst_2)
                 distances.append((wire_lengths[0] + extraDst_1, wire_lengths[1] + extraDst_2))
         else:   # segment 1 must be vertical, match x1 to y2
             x_1 = start_1[0]
@@ -70,14 +72,14 @@ for seg1 in segments[0]:
             yMin = min(start_1[1], end_1[1])
             # Check if the segments intersect
             if ((xMin < x_1 and x_1 < xMax) and (yMin < y_2 and y_2 < yMax)):
-                print("Vertical Intersection:", start_1, "->", end_1, "with", start_2, "->", end_2)
+                #print("Vertical Intersection:", start_1, "->", end_1, "with", start_2, "->", end_2)
                 intersections.append((x_1,y_2))
                 num_intersections += 1
                 extraDst_1 = abs(start_1[1] - y_2)
                 extraDst_2 = abs(start_2[0] - x_1)
-                print("Wire 1 length to bend", wire_lengths[0],"+",extraDst_1)
-                print("Wire 2 length to bend", wire_lengths[1],"+",extraDst_2)
-                print("sum = ", wire_lengths[0] + extraDst_1 + wire_lengths[1] + extraDst_2)
+                #print("Wire 1 length to bend", wire_lengths[0],"+",extraDst_1)
+                #print("Wire 2 length to bend", wire_lengths[1],"+",extraDst_2)
+                #print("sum = ", wire_lengths[0] + extraDst_1 + wire_lengths[1] + extraDst_2)
                 distances.append((wire_lengths[0] + extraDst_1, wire_lengths[1] + extraDst_2))
                 
         start_2 = end_2
