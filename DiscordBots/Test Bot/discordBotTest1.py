34312b44aa2f4a84f4f9845d9693f48bc0e943e9
Ethan Kessel <the.rednecksidekick@gmail.com>
2020-04-27 14:42:07 -0700

    Basic log on message

--- DiscordBots/Test Bot/discordBotTest1.py
+++ DiscordBots/Test Bot/discordBotTest1.py
@@ -7,12 +7,22 @@ Created on Sun Mar  8 23:29:37 2020
 
 import discord
 
-token = 'Njg2NDU2MzQ0MzQ1MzEzMjgx.XmXg0Q.Rzdec-3QZmrKwNREDJ_VRrwMDh0'
+token = 'Njg2NDU2MzQ0MzQ1MzEzMjgx.XmXmoA.ROvkeLojSRmo_7KWyhWQCRpJPzU'
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+    welcome_channel_target = None
+    for chnl in client.get_all_channels():
+        print('Channel \'{0}\' is {1}'.format(chnl, type(chnl)))
+        if (chnl.type == discord.ChannelType.text):
+            if (chnl.position == 0):
+                welcome_channel_target = chnl
+
+    client.get_channel
+    await welcome_channel_target.send('{0.user}: Logged on.'.format(client), tts=True)
 
 @client.event
 async def on_message(message):
@@ -22,4 +32,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('Njg2NDU2MzQ0MzQ1MzEzMjgx.XmXg0Q.Rzdec-3QZmrKwNREDJ_VRrwMDh0')
+client.run(token)
